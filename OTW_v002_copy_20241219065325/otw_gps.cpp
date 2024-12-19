5ebbe3b82c25ac7cf1dc6670f995031aac5765cd
PhoenixJack <35715826+Phoenixjack@users.noreply.github.com>
2024-12-19 17:12:17 -0500

    Update otw_gps.cpp

--- OTW_v002_copy_20241219065325/otw_gps.cpp
+++ OTW_v002_copy_20241219065325/otw_gps.cpp
@@ -14,21 +14,20 @@ struct gps {
   uint8_t fixquality = 0;              //
   bool lock = false;                   //
   bool pps_sync_in_progress = false; // we'll set this flag while listening 
-  uint32_t micros_capture = 0; // we'll store the micros() here in the interrupt routine 
+  uint32_t micros_capture = 0; // we'll store the micros() here in the interrupt routine
   uint32_t epoch_time_sec = 0;             // holds GPS time as epoch timestamp in seconds
   uint32_t epoch_time_usec = 0; // will hold the LSB portion of epoch time
   uint32_t next_time_sync = 0;         // holds the next millis() when we'll conduct a epoch calculation and sync to PPS
   bool init();
   bool check();
-  void sync_time();
-  void start_pps_sync();
+  void pps_sync_enable();
+  void pps_sync_disable();
   void pps_sync_ISR();
-  void check_and_act();
+  uint64_t getEpochTime_usec();
   void stage_data();
 } gps;
 
-bool gps::init() {                // initialize the port and try to configure it
-attachInterrupt(digitalPinToInterrupt(GPS_PPS_IN), pps_sync_ISR, RISING); GPS.begin(9600);                                // usually 4800 or 9600 baud
+bool gps::init() {                // initialize the port and try to configure it GPS.begin(9600);                                // usually 4800 or 9600 baud
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);   // may not work with all gps modules
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);  // ditto
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);      // 1 Hz update rate
@@ -36,57 +35,84 @@ attachInterrupt(digitalPinToInterrupt(GPS_PPS_IN), pps_sync_ISR, RISING); GPS.be
   ready = false;    // used in case we need to re-initialize GPS
   while (millis() < timeout && !ready) {          // as long as we haven't timed out AND we haven't recieved a proper GPS sentence
     ready = check();             // keep checking
-    start_pps_sync();
   }
   return ready;
 };
 
-bool gps::check() {  // TODO: here is where we'll check next_time_sync and then enable interrupts, get PPS, disable interrupts and update the epoch time
+bool gps::check() {
   char c = GPS.read();
   last_fix_sec = (uint8_t)GPS.secondsSinceFix();
-  newdata = GPS.newNMEAreceived();
-  if (last_fix_sec > 10) { newdata = false; } else { stage_data(); }
+  newdata = GPS.newNMEAreceived(); // process any data received so far
+  if (last_fix_sec > 10) {
+    newdata = false; // we've timed out 
+    gps_lock = false; // and lost the fix
+  } else { 
+    stage_data(); 
+  }
+  if (millis() > next_time_sync) {
+    if (pps_sync_in_progress) {
+      if (micros_capture != 0) { 
+        pps_sync_disable(); // captured, so end it
+      }
+    } else {
+      pps_sync_enable(); // it's time to start listening
+    }
+  }
   return newdata;
 };
 
-void gps::sync_time(){
-  pps_sync_in_progress
-// get PPS 
-// set flag
-// disable interrupts
-  
+void gps::pps_sync_enable() {
+  pps_sync_in_progress = true;
+  micros_capture = 0; attachInterrupt(digitalPinToInterrupt(GPS_PPS_IN), pps_sync_ISR, RISING);
 };
 
-void gps::start_pps_sync() {
-  pps_sync_in_progress = true;
-  interrupts(); // enable interrupts
+void gps::pps_sync_disable() {
+ detachInterrupt(digitalPinToInterrupt (GPS_PPS_IN)); // disable interrupts so it doesn't happen again 
+  epoch_time_usec = micros() - micros_capture;
+  pps_sync_in_progress = false; // clear the flag
+  if (GPS.year > 0) {   // I got 99 problems, but this bitch ain't one
+    stamp.setDateTime(GPS.year + 2000, GPS.month, GPS.day, GPS.hour, GPS.minute, GPS.seconds);
+    epoch_time = stamp.getUnix();
+    if (epoch_time_usec > 1000000) { // if it's been more than a second since we captured pps
+      epoch_time += (epoch_time_usec/1000000);
+      epoch_time_usec %= 1000000;
+    }
+  } else {
+    epoch_time = 0; // it's invalid, so ditch the value
+    epoch_time_usec = 0; // ditto
+  }
+  next_time_sync = millis() + 120000; 
 };
 
 void gps::pps_sync_ISR() {
-  pps_sync_in_progress = false;
-  micros_capture = micros();
+  if (pps_sync_in_progress) { // just in case the ISR gets called again before postprocessing is complete
+    micros_capture = micros();
+    pps_sync_in_progress = false;
+  }
 };
 
+uint64_t gps::getEpochTime_usec() { 
+  if (epoch_time > 0) {
+    uint64_t time_value = (uint64_t)epoch_time * 1000000; // leave room for msec and usec
+    if (epoch_time_usec > 0) {
+      // time_value += (uint64_t)
+    }
+  } else {
+    return 0;
+  } 
+};
 void gps::stage_data() { 
   time_stamp = millis();
-
-
-  if (GPS.year == 0) {               // it's possible to get NMEA sentences WITHOUT even a timefix yet; if we don't even have that, don't set epoch time
-    epoch_time = 0;           // this is Chris telling Chris that Chris has a problem
-  } else {                           // I got 99 problems, but this ain't one
-    stamp.setDateTime(GPS.year + 2000, GPS.month, GPS.day, GPS.hour, GPS.minute, GPS.seconds);
-    epoch_time = stamp.getUnix();
-  }
   if (GPS.parse(GPS.lastNMEA())) {  // only load GPS data if we have a GPS fix
-    report.gps_lock = GPS.fix;
-    report.fixquality = GPS.fixquality;
-    if (report.gps_lock) {  // only load GPS data if we have a GPS lock
-      report.lat = GPS.latitudeDegrees;
-      report.lon = GPS.longitudeDegrees;
-      report.altitude = GPS.altitude;
+    gps_lock = GPS.fix;
+    fixquality = GPS.fixquality;
+    if (gps_lock) {  // only load GPS data if we have a GPS lock
+      lat = GPS.latitudeDegrees;
+      lon = GPS.longitudeDegrees;
+      altitude = GPS.altitude;
     } else {
-      report.lat = 0;
-      report.lon = 0;
+      lat = 0;
+      lon = 0;
     }
   }
 };
